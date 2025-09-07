70f6d347f8344d5c0d45413144cb7bdb0ae257a4
anantgillagrofarm-code <anantgillagrofarm@gmail.com>
2025-09-08 03:49:23 +0530

    main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,10 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import App from './App'
-import Admin from './pages/Admin'
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/admin/*" element={<Admin/>} />
-        <Route path="/*" element={<App/>} />
-      </Routes>
-    </BrowserRouter>
+    <App />
   </React.StrictMode>
 )
