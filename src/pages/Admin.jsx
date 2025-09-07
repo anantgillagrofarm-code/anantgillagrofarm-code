e614cf7d64ba80a1aaca75c46897e61da4d89b65
anantgillagrofarm-code <anantgillagrofarm@gmail.com>
2025-09-08 02:41:35 +0530

    add Admin.jsx (admin placeholder)

---
+++ src/pages/Admin.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Admin(){
+  return (
+    <div style={{padding:20}}>
+      <h2>Admin Panel (placeholder)</h2>
+      <p>When Firebase is connected, this area will let you add/edit products, change prices and stock.</p>
+    </div>
+  )
+}
