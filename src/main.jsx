e7610769d1c787d86f779f86257a5154309a7cc2
anantgillagrofarm-code <anantgillagrofarm@gmail.com>
2025-09-15 01:08:59 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
-import React from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App'
-import './index.css'
+// src/main.jsx
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
 
-createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <div className="app-root">
+      <App />
+    </div>
   </React.StrictMode>
-)
+);
