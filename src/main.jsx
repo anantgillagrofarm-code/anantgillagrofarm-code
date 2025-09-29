af459fc936c005b39bb90ce15dcee61709bd873a
anantgillagrofarm-code <anantgillagrofarm@gmail.com>
2025-09-29 16:21:10 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,18 @@
 // src/main.jsx
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import App from './App';
-import './index.css'; // REQUIRED - ensures styles are loaded
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App";
+import "./index.css";
 
-const root = createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const rootElement = document.getElementById("root");
+if (!rootElement) {
+  // helpful error message in case index.html doesn't have a #root
+  console.error("Unable to find #root element. Ensure public/index.html contains an element with id=\"root\".");
+} else {
+  const root = createRoot(rootElement);
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+}
