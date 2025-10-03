d58c425b588e39bdf0dfbf68009c386136aa29ee
anantgillagrofarm-code <anantgillagrofarm@gmail.com>
2025-10-03 19:08:03 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: './', // 👈 IMPORTANT for Vercel (relative paths)
+  base: '/', // 👈 IMPORTANT for Vercel (relative paths)
   build: {
     outDir: 'dist',
   },
