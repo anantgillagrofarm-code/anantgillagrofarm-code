c7d38f70f44ae4c5519d1f67edd4c8d2af659ea7
anantgillagrofarm-code <anantgillagrofarm@gmail.com>
2025-10-03 20:28:31 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,7 @@
 // src/main.jsx
-import React from "react";
-import { createRoot } from "react-dom/client";
-import App from "./App";
-import "./index.css";
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App';
+// import './index.css'; // <-- COMMENT THIS LINE OUT
 
-const rootEl = document.getElementById("root");
-if (!rootEl) {
-  throw new Error('No root element found. Make sure "index.html" has <div id="root"></div>');
-}
-
-createRoot(rootEl).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// ... rest of the file ...
