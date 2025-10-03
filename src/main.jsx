9aa835c7475977277042d04c3a7ed1126eb1d3a2
anantgillagrofarm-code <anantgillagrofarm@gmail.com>
2025-10-03 21:06:09 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,18 @@
-// src/main.jsx
+// src/main.jsx - FINAL, FINAL TEST
+
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
-// import './index.css'; // <-- COMMENT THIS LINE OUT
+// import "./index.css"; // KEEP COMMENTED OUT
+// (The rest of your file is below)
 
-// ... rest of the file ...
+const rootEl = document.getElementById("root");
+if (!rootEl) {
+  throw new Error('No root element found. Make sure index.html has a root element.');
+}
+
+createRoot(rootEl).render(
+  // <React.StrictMode> <--- COMMENT THIS OUT
+    <App />
+  // </React.StrictMode> <--- COMMENT THIS OUT
+);
